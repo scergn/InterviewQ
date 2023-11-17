e6e92eef53fc9f9b198a8a1fdc0b047158c80c85
scergun <erguncan88@gmail.com>
2023-11-17 06:13:59 +0300

    Interview Qs

--- src/Interview_Questions/Interview_Questions/Q52_ReadFileTxt.java
+++ src/Interview_Questions/Interview_Questions/Q52_ReadFileTxt.java
@@ -1,4 +1,4 @@
-package interviewQuestions;
+package Interview_Questions.Interview_Questions;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,8 +19,22 @@ public class Q52_ReadFileTxt {
    */
 
     public static void main(String[] args) throws IOException {
-
-
-
+        Map<String,Integer> wordCounter=new HashMap<>();
+        List<String> lines=Files.readAllLines(Paths.get("src/interviewQuestions/Q52_FileText"));
+        System.out.println(lines);
+        for (String w:lines) {
+            String[] words=w.split(" ");
+            for (String m:words) {
+                if (m.endsWith(".")||m.endsWith(",")||m.endsWith("?")){
+                    m=m.substring(0,m.length()-1);
+                }
+                if (!wordCounter.containsKey(m)){
+                    wordCounter.put(m,1);
+                }else {
+                    wordCounter.put(m,wordCounter.get(m)+1);
+                }
+            }
+        }
+        System.out.println(wordCounter);
     }
 }
